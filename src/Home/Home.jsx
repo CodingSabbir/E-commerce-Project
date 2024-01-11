9c95d77c963937da454eddd8cef43b560e1d21e4
MD.Sabbir Rahman <mdsabbirrahmanm740@gmail.com>
2024-01-11 20:24:18 +0600

    regester

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner';
 import HomeCetegory from './HomeCetegory';
 import CategoryProduct from './CategoryProduct';
+import Regester from './Regester';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner/>
             <HomeCetegory />
             <CategoryProduct/>
+            <Regester/>
         </div>
     );
 };
